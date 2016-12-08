36f1bb95a83af6bf2f7d87a5cfbe2db69b1b4c52
lynchy901 <nml5182@psu.edu>
2016-12-07 19:37:11 -0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <Header/>
+         </div>
+      );
+   }
+}
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
+         </div>
+      );
+   }
+}
+
+export default App;
